377b185513158d12e0a1739e965b7544de983b44
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-28 21:16:36 +0200

    Move google provider to another file

--- api/urls.py
+++ api/urls.py
@@ -4,26 +4,17 @@ from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_swagger.views import get_swagger_view
 
-from github_projects_tracker.settings import env
+from api.social_auth import GithubLogin
 
 router = DefaultRouter()
 # router.register('dishes', DishModelViewSet, basename='dishes')
 # router.register('menus', MenuModelViewSet, basename='menus')
 
 
-from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from dj_rest_auth.registration.views import SocialLoginView
 
-
-class GithubLogin(SocialLoginView):
-    adapter_class = GitHubOAuth2Adapter
-    callback_url: str = env('CALLBACK_URL')
-    client_class = OAuth2Client
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/token-auth/', views.obtain_auth_token),
-    path('v1/register/', GithubLogin.as_view(), name='github_login'),
-    url('v1/documentation/', get_swagger_view(title='Pastebin API')), # TODO
+    path('v1/login/', GithubLogin.as_view(), name='github_login'), # todo better url
+    url('v1/documentation/', get_swagger_view(title='API Endpoints')),
 ]
