933cd5500a8df52eb6989c7f93337153d0aabdf3
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-28 21:05:42 +0200

    setup swagger and env

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_swagger.views import get_swagger_view
 
+from github_projects_tracker.settings import env
+
 router = DefaultRouter()
 # router.register('dishes', DishModelViewSet, basename='dishes')
 # router.register('menus', MenuModelViewSet, basename='menus')
@@ -13,14 +15,15 @@ from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 
+
 class GithubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
-    callback_url = 'http://localhost:8000/accounts/github/login/callback/'
+    callback_url: str = env('CALLBACK_URL')
     client_class = OAuth2Client
 
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/token-auth/', views.obtain_auth_token),
     path('v1/register/', GithubLogin.as_view(), name='github_login'),
-    #url('v1/documentation/', get_swagger_view(title='Pastebin API')),
+    url('v1/documentation/', get_swagger_view(title='Pastebin API')), # TODO
 ]
--- github_projects_tracker/urls.py
+++ github_projects_tracker/urls.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from dj_rest_auth.registration.views import SocialLoginView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
