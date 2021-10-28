a739b38d6fe2c8c3b07bf12f2577fee597085aad
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-28 20:35:00 +0200

    setup api

--- github_projects_tracker/urls.py
+++ github_projects_tracker/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 
-class GithubLogin(SocialLoginView):
-    adapter_class = GitHubOAuth2Adapter
-    callback_url = 'http://localhost:8000/accounts/github/login/callback/'
-    client_class = OAuth2Client
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dj-rest-auth/github/', GithubLogin.as_view(), name='github_login'),
+    path('api/', include('api.urls')),
 ]
