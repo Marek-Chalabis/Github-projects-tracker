a17d313062a7c5c6a05c8a5c308e6e769dc33d61
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-29 21:07:32 +0200

    Add urls for api

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,16 @@
 from django.conf.urls import url
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_swagger.views import get_swagger_view
 
 from api.social_auth import GithubLogin
+from api.views import ProjectModelViewSet
 
 router = DefaultRouter()
-# router.register('dishes', DishModelViewSet, basename='dishes')
-# router.register('menus', MenuModelViewSet, basename='menus')
-
-
-
+router.register('projects', ProjectModelViewSet, basename='projects')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/login/', GithubLogin.as_view(), name='github_login'), # todo better url
     url('v1/documentation/', get_swagger_view(title='API Endpoints')),
+    path('v1/login_github/', GithubLogin.as_view(), name='login_github'),
 ]
