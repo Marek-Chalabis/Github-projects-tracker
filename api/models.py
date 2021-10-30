35b9f912abe754dca84e52f84b509c93e7fd4022
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-30 10:10:06 +0200

    Add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from allauth.socialaccount.models import SocialAccount, SocialToken
+from crum import get_current_user
+
+
+class Project(models.Model):
+
+    added_by = models.ForeignKey(
+        'auth.User',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='%(class)s_added_by',
+    )
+    project_name = models.CharField(max_length=80, unique=True)
+
+    def save(self, *args, **kwargs):
+        """Store user who added project to monitor"""
+        if not self.pk:
+            self.added_by = get_current_user()
+        super().save(*args, **kwargs)
