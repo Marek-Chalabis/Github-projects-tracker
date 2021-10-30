aeea28eeeb401acfcdfcc68ed2995823d661e584
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-30 15:10:44 +0200

    Add url for repository download

--- api/github_organization_api/github_api_urls.py
+++ api/github_organization_api/github_api_urls.py
@@ -1,3 +1,6 @@
 URL_GET_REPOSITORY = 'https://api.github.com/repos/{owner}/{repo}'
 URL_GET_COMMIT = 'https://api.github.com/repos/{owner}/{repo}/commits/{branch}'
 URL_GET_LIST_ORGANIZATION_REPOSITORIES = 'https://api.github.com/orgs/{org}/repos'
+URL_DOWNLOAD_REPOSITORY_ARCHIVE_ZIP = (
+    'https://api.github.com/repos/{owner}/{repo}/zipball'
+)
