f0e3cc65d1374700882295d303c0e9de437cd6ff
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-30 15:44:31 +0200

    Add tests models

---
+++ api/tests/test_models.py
@@ -0,0 +1,13 @@
+import pytest
+from django.contrib.auth.models import User
+
+from api.models import Project
+
+
+@pytest.mark.django_db()
+class TestBaseCycle:
+    def test_project_added_by(self, mocker):
+        test_user = User.objects.create(username='testuser')
+        mocker.patch('api.models.get_current_user', return_value=test_user)
+        test_project = Project.objects.create(project_name='Test1')
+        assert test_project.added_by == test_user
