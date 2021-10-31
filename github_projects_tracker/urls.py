169d2693181be0e72bd34d80344e3cf93ba16971
Marek Chałabis <marek.chalabis@tylko.com>
2021-10-31 19:10:41 +0100

    Allow django toolbar

--- github_projects_tracker/urls.py
+++ github_projects_tracker/urls.py
@@ -4,7 +4,13 @@ from django.urls import (
     path,
 )
 
+from github_projects_tracker import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+if settings.DEBUG and settings.DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
